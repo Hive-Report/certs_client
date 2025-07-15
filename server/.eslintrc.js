2d7ff3ef0da76a754c293e12cb5bbe7ce4e70262
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-07-15 19:39:36 +0300

    Add initial project setup with configuration files and scripts

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+const { parse } = require("path");
+
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+    rules: {
+        
+    }
+};
