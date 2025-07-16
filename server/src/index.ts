d8313599b68a6227e38d8c8f159398546ac46876
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-07-16 13:04:46 +0300

    feat: initialize logger and update server startup to include logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { config } from './config.js';
 import cors from 'cors';
 import router from './router/index.js';
+import { createLogger } from './logger/index.js';
 
 const app = express();
-const port = config.PORT;
+const port = config.PORT
+const logger = createLogger("CertsAPI");
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', router);
+
+app.listen(port, () => {
+  logger.info(`Server is running on PORT: ${port}`);
+});
